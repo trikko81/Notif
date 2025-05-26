7260712702995747ac72af236c1cc2ae298cdf21
John E. Napoles <artismartis3@gmail.com>
2025-05-26 10:08:08 -0400

    changed format

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ gameText = tk.Label(
     root,
     text="youve been on xxxxxxxxx for 45 Minutes, you might wanna take a break",
     font = ("Roboto",12),
-    wraplength=225,
+    wraplength=200,
     justify="left"
     )
 
@@ -31,12 +31,11 @@ logo.image = photo
 
 
 root.columnconfigure((0,1,2),weight=1)
-root.rowconfigure(0,weight=1)
-root.rowconfigure(1,weight=1)
+root.rowconfigure((0,1),weight=1)
 
 
 gameText.grid(row=1,column=0,sticky="nw",columnspan=2, padx=10)
-logo.grid(row=0,column=3, sticky="ns")
+logo.grid(row=0,column=3, rowspan=2,  sticky="ns")
 
 def NotifSlide():
     global x
