1c10c5addf58b3580a962932d8987ed99e3bdbeb
John E. Napoles <artismartis3@gmail.com>
2025-05-31 17:55:46 -0400

    it popups every 15 minutes

--- main.py
+++ main.py
@@ -2,63 +2,65 @@ import tkinter as tk
 from tkinter import PhotoImage
 from PIL import Image, ImageTk
 import pygetwindow as gw # type: ignore
+import time
+
+
 active_window = gw.getActiveWindow()
 print(active_window.title)
+def create_popup():
+    root = tk.Tk()
+    window_width = 300
+    window_height = 100
+    screen_width = root.winfo_screenwidth()
+    screen_height = root.winfo_screenheight()
+    x = screen_width - window_width + 300   
+    y = screen_height - window_height - 75
+    root.geometry(f"{window_width}x{window_height}+{x}+{y}")
+    root.resizable(False, False)
+    root.overrideredirect(True) 
+
+    gameText = tk.Label(
+        root,
+        text=f"youve been on {active_window.title} for 45 Minutes, you might wanna take a break",
+        font = ("Roboto",12),
+        wraplength=200,
+        justify="left"
+        )
 
 
-root = tk.Tk()
-window_width = 300
-window_height = 100
-screen_width = root.winfo_screenwidth()
-screen_height = root.winfo_screenheight()
-x = screen_width - window_width + 300   
-y = screen_height - window_height - 75
-root.geometry(f"{window_width}x{window_height}+{x}+{y}")
-root.resizable(False, False)
-root.overrideredirect(True) 
 
-gameText = tk.Label(
-    root,
-    text=f"youve been on {active_window.title} for 45 Minutes, you might wanna take a break",
-    font = ("Roboto",12),
-    wraplength=200,
-    justify="left"
-    )
+    root.columnconfigure(1,weight=1)
 
 
-preSize = Image.open("logo/terraria.png")
-resized = preSize.resize((75,75), Image.Resampling.LANCZOS)
-photo = ImageTk.PhotoImage(resized)
-logo = tk.Label(root, image=photo)
-logo.image = photo 
+    gameText.grid(row=1,column=0,sticky="nw",columnspan=2, padx=10)
+    def NotifSlide():
+        global x
+        x -= 1 
+        if x > screen_width - window_width: 
+            root.geometry(f"{window_width}x{window_height}+{x}+{y}")
+            root.after(2, NotifSlide)
+        else:
+            root.geometry(f"{window_width}x{window_height}+{screen_width - window_width}+{y}")
+            root.after(5000, NotifClose)
+    def NotifClose():
+        global x
+        x += 1 
+        if x <= screen_width:
+            root.geometry(f"{window_width}x{window_height}+{x}+{y}")
+            root.after(2, NotifClose)
+        else:
+            root.destroy()
+            
+    while True :
+        time.sleep(15*60)
+        NotifSlide()
 
+def schedule_popup():
+    create_popup()
+    root_scheduler.after(15 * 60 * 1000, schedule_popup)  
 
-root.columnconfigure((0,1,2),weight=1)
-root.rowconfigure((0,1),weight=1)
-
-
-gameText.grid(row=1,column=0,sticky="nw",columnspan=2, padx=10)
-logo.grid(row=0,column=3, rowspan=2,  sticky="ns")
-
-def NotifSlide():
-    global x
-    x -= 1 
-    if x > screen_width - window_width: 
-        root.geometry(f"{window_width}x{window_height}+{x}+{y}")
-        root.after(2, NotifSlide)
-    else:
-        root.geometry(f"{window_width}x{window_height}+{screen_width - window_width}+{y}")
-        root.after(5000, NotifClose)
-def NotifClose():
-    global x
-    x += 1 
-    if x <= screen_width:
-        root.geometry(f"{window_width}x{window_height}+{x}+{y}")
-        root.after(2, NotifClose)
-    else:
-        root.destroy()
-        
-
-NotifSlide()
-root.mainloop()
+root_scheduler = tk.Tk()
+root_scheduler.withdraw()  
+schedule_popup()
+root_scheduler.mainloop()
 
